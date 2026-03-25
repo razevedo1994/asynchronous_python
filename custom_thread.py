fd6a3c1e794c3f296d9ddc093c90504c08d80408
Rodrigo <razevedo.contato@gmail.com>
2026-03-25 08:45:11 -0300

    Custom thread to abserve race condition

---
+++ custom_thread.py
@@ -0,0 +1,17 @@
+import time
+import threading
+from typing import Mapping
+
+class Mayhem(threading.Thread):
+    def __init__(self, map: Mapping[str, int]) -> None:
+        super().__init__()
+        self.map = map
+
+    def run(self):
+        for key, value in self.map.items():
+            time.sleep(value)
+
+d = {"k1": 1, "k2": 2, "k3": 3}
+m = Mayhem(d)
+m.start()
+d["k4"] = 4
